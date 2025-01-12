14a5248ffd11cee0bc7faa2387000874329422dc
Fernando Torres <jose_fer_torres@hotmail.com>
2025-01-11 18:05:57 -0600

    docs: Se realizaron cambios en la documentación.

--- Web/Models/AccountViewModels.cs
+++ Web/Models/AccountViewModels.cs
@@ -55,15 +55,15 @@ public class LoginViewModel
     [Required]
     [Display(Name = "Correo electrónico")]
     [EmailAddress]
-    public string Email { get; set; }
+    public required string Email { get; set; }
 
     [Required]
     [DataType(DataType.Password)]
     [Display(Name = "Contraseña")]
-    public string Password { get; set; }
+    public required string Password { get; set; }
 
     [Display(Name = "¿Recordar cuenta?")]
-    public bool RememberMe { get; set; }
+    public bool RememberMe { get; set; } = false;
 }
 
 public class RegisterViewModel
